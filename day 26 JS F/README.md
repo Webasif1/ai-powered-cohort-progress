ad785f6a8f5ce4c5fafdf975338fe7e051ba3c8a
webasif <mdh606392@gmail.com>
2025-10-24 22:01:14 +0600

    basic of js

---
+++ day 26 JS F/README.md
@@ -0,0 +1,2 @@
+How to learn js and task
+
---
+++ day 26 JS F/script.js
@@ -0,0 +1,13 @@
+alert("Hello!");
+console.log("Hello!");
+prompt("Enter Your Name");
+// String
+"My name is asif"
+
+// Slice
+"How are you Asif".slice(0,9);
+// `` this id backtick or template literal
+
+console.log("I hope your will".split(i).join());
+console.log("I hope your will".replace(i, all));
+console.log("I hope your will".replaceAll(i, all));

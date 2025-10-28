1004ff1fe3821b54393826c7760942c6f9d8b6db
WebAsif1 <mdasif606392@gmail.com>
2025-10-28 22:28:06 +0600

    day 29 js practice like data type more and more

--- day 29 JS F/script.js
+++ day 29 JS F/script.js
@@ -16,17 +16,29 @@
 // console.warn("Dhaka");
 // console.error("Do not!");
 
-let nm = prompt('Enter your name');
+// let nm = prompt('Enter your name');
 // alert(`Welcome ${nm}!`);
-alert("Welcome" + " " + nm +"!");
+// alert("Welcome" + " " + nm +"!");
 
-// let age = prompt("Enter age:");
+// let age = +prompt("Enter age:"); // prompt give us string so  to convert string to number add a + sign or Number();
 // console.log(age + 5);
 
-let msg = "I love Sheryians";
-console.log(msg);
-let msg1 = msg.slice(2, 6);
-console.log(msg1);
+// let msg = "I love Sheryians";
+// console.log(msg);
 
-let msg2 = msg.replace("love", "study at");
-console.log(msg2);
+// let msg1 = msg.slice(2, 6);
+// console.log(msg1);
+
+// let msg2 = msg.split("");
+// console.log(msg2);
+
+// let msg3 = msg.replace("love", "study at");
+// console.log(msg3);
+
+
+// let obj1 = { name: "Harsh" };
+// let obj2 = obj1;
+// obj2.name = "Sheryians";
+// console.log(obj1.name)
+// console.log(obj1);
+

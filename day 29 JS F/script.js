d68f0d787a9a0f39fad3598fee53c61f56efc046
WebAsif1 <mdasif606392@gmail.com>
2025-10-28 21:52:31 +0600

    day 29 js practice

---
+++ day 29 JS F/script.js
@@ -0,0 +1,32 @@
+// var a = "Asif";
+// let b = "Sheryianss";
+// const c = "School";
+
+// a = "Updated";
+// b = "Updated";
+// c = "Updated";
+
+// {
+//   let d = "Hello";
+// }
+// console.log(d);
+
+// console.log("Asif");
+// console.info(23);
+// console.warn("Dhaka");
+// console.error("Do not!");
+
+let nm = prompt('Enter your name');
+// alert(`Welcome ${nm}!`);
+alert("Welcome" + " " + nm +"!");
+
+// let age = prompt("Enter age:");
+// console.log(age + 5);
+
+let msg = "I love Sheryians";
+console.log(msg);
+let msg1 = msg.slice(2, 6);
+console.log(msg1);
+
+let msg2 = msg.replace("love", "study at");
+console.log(msg2);

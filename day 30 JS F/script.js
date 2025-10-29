93bea6738c5ab8c5be7f54f28aba52f5591219b9
WebAsif1 <mdasif606392@gmail.com>
2025-10-29 22:07:53 +0600

    day 30 js f

---
+++ day 30 JS F/script.js
@@ -0,0 +1,11 @@
+// condition ? do this(if true run it) : do that(if false run this)
+12 > 11 ?console.log("hey") : console.log("hello");
+
+//  TypeOff
+// 12 typeof
+
+// sting add by +
+"araft" + "hossain"
+
+?? oparator
+// null ?? console.log("user does not exist");

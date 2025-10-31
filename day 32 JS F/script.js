5bb77388cfc26ee8a538bc1e0cd332161c5a8cbe
WebAsif1 <mdasif606392@gmail.com>
2025-10-31 21:43:26 +0600

    Condition and switch

---
+++ day 32 JS F/script.js
@@ -0,0 +1,24 @@
+//** 0 "" false NaN null undefined document.all => if any of them in side the if condition then it will false
+//** if(should true or false ){}
+//** Condition ? true : false
+// 12>13 ? console.log("hey") : console.log("hello");
+
+//**Switch
+// switch(2){
+//   case 1:
+//   console.log("hey");
+//   break;
+//   case 2:
+//   console.log("hey hey");
+//   break;
+//   case 3:
+//   console.log("hey hey hey");
+//   break;
+//   case 4:
+//   console.log("hey hey hey hey");
+//   break;
+//   default:
+//   console.log("default case");
+// }
+
+

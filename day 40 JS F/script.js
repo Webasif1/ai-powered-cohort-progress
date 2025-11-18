c961a9d6c87984e3100936eca0b256a7ff8dea95
WebAsif1 <mdasif606392@gmail.com>
2025-11-18 17:01:58 +0600

    switch case question

--- day 40 JS F/script.js
+++ day 40 JS F/script.js
@@ -539,32 +539,32 @@
 //  Conditionals & Loops
 //  21. Write a switch statement that logs the weekday based on a number (1–7).
 
-let day = 2;
-switch(day){
-      case 1:
-      console.log("Saturday");
-      break;
-      case 2:
-      console.log("Sunday");
-      break;
-      case 3:
-      console.log("Monday");
-      break;
-      case 4:
-      console.log("Tuesday");
-      break;
-      case 5:
-      console.log("Wednesday");
-      break;
-      case 6:
-      console.log("Thrust day");
-      break;
-      case 7:
-      console.log("Friday");
-      break;
-      default:
-      console.log("Invalid day");
-}
+// let day = 2;
+// switch(day){
+//       case 1:
+//       console.log("Saturday");
+//       break;
+//       case 2:
+//       console.log("Sunday");
+//       break;
+//       case 3:
+//       console.log("Monday");
+//       break;
+//       case 4:
+//       console.log("Tuesday");
+//       break;
+//       case 5:
+//       console.log("Wednesday");
+//       break;
+//       case 6:
+//       console.log("Thrust day");
+//       break;
+//       case 7:
+//       console.log("Friday");
+//       break;
+//       default:
+//       console.log("Invalid day");
+// }
 
 
 

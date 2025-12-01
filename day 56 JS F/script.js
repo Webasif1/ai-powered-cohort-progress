025c76a402742c0ee6b6fa0c8286a48522dee58c
WebAsif1 <mdasif606392@gmail.com>
2025-12-01 22:23:48 +0600

    Object Oriented Concept in js

---
+++ day 56 JS F/script.js
@@ -0,0 +1,15 @@
+// Object Oriented Concept in js
+
+
+// Class
+
+class BiscuitMaker{
+  constructor(){
+    this.name = "ParleG";
+    this.price = 5;
+  }
+}
+
+let biscuitMaker = new BiscuitMaker();
+console.log("biscuitMaker");
+console.log(biscuitMaker);

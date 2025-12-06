e945829fc10725828d16ad0c0b7d4af4a861aef4
WebAsif1 <mdasif606392@gmail.com>
2025-12-06 22:17:55 +0600

    callback

---
+++ day 59 JS F/script.js
@@ -0,0 +1,11 @@
+// Object Oriented Concept in js
+
+// async and
+//callback
+function iceCreamLaao(address, fn){
+  fn({lang:124134.34, long:141434})
+}
+
+iceCreamLaao("Dhaka",function(det){
+  console.log(det);
+})

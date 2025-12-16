86c05f35dfc44dc89eca4c2d1eaf571a70903314
WebAsif1 <mdasif606392@gmail.com>
2025-12-16 14:21:19 +0600

    todo page on going

--- Projects/project 01 HCJ F/app.js
+++ Projects/project 01 HCJ F/app.js
@@ -1,18 +1,21 @@
-var allElem = document.querySelectorAll(".elem");
-var fullElemPage = document.querySelectorAll(".fullElem")
-var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back")
+function openFeature() {
+    var allElem = document.querySelectorAll(".elem");
+    var fullElemPage = document.querySelectorAll(".fullElem");
+    var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElem.forEach(function(elem,indx){
-  elem.addEventListener("click", function(){
-    // console.log(elem);
-    console.log(fullElemPage[indx])
-    fullElemPage[indx].style.display = "block";
-  })
-})
+    allElem.forEach(function (elem, indx) {
+        elem.addEventListener("click", function () {
+            // console.log(elem);
+            console.log(fullElemPage[indx]);
+            fullElemPage[indx].style.display = "block";
+        });
+    });
 
-fullElemPageBackBtn.forEach(function(back, indx){
-  back.addEventListener("click", function(){
-    console.log(indx);
-    fullElemPage[indx].style.display = "none";
-  })
-})
+    fullElemPageBackBtn.forEach(function (back, indx) {
+        back.addEventListener("click", function () {
+            console.log(indx);
+            fullElemPage[indx].style.display = "none";
+        });
+    });
+}
+openFeature()

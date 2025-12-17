3bbd9aa490edb7c6126fae9541eff9f03b524558
WebAsif1 <mdasif606392@gmail.com>
2025-12-17 20:29:44 +0600

    set in local storage all task

--- Projects/project 01 HCJ F/app.js
+++ Projects/project 01 HCJ F/app.js
@@ -1,21 +1,66 @@
 function openFeature() {
-    var allElem = document.querySelectorAll(".elem");
-    var fullElemPage = document.querySelectorAll(".fullElem");
-    var fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
+  const allElem = document.querySelectorAll(".elem");
+  const fullElemPage = document.querySelectorAll(".fullElem");
+  const fullElemPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-    allElem.forEach(function (elem, indx) {
-        elem.addEventListener("click", function () {
-            // console.log(elem);
-            console.log(fullElemPage[indx]);
-            fullElemPage[indx].style.display = "block";
-        });
+  allElem.forEach(function (elem, indx) {
+    elem.addEventListener("click", function () {
+      // console.log(elem);
+      console.log(fullElemPage[indx]);
+      fullElemPage[indx].style.display = "block";
     });
+  });
 
-    fullElemPageBackBtn.forEach(function (back, indx) {
-        back.addEventListener("click", function () {
-            console.log(indx);
-            fullElemPage[indx].style.display = "none";
-        });
+  fullElemPageBackBtn.forEach(function (back, indx) {
+    back.addEventListener("click", function () {
+      console.log(indx);
+      fullElemPage[indx].style.display = "none";
     });
+  });
 }
-openFeature()
+openFeature();
+
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form #task-input");
+let taskDetailInput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #check");
+const allTask = document.querySelector(".allTask");
+
+console.log(allTask);
+let currentTask = [];
+
+if(localStorage.getItem("currentTask")){
+    currentTask = JSON.parse(localStorage.getItem("currentTask"))
+}else{
+    console.log("Current task is empty");
+}
+
+function renderTask() {
+  let sum = "";
+  currentTask.forEach((elem) => {
+    sum += `<div class="task">
+                        <h5>${elem.task} <span class="${elem.imp}">Imp</span></h5>
+                        <button>Mark as completed</button>
+                    </div>`;
+  });
+  allTask.innerHTML = sum;
+}
+renderTask();
+
+// console.log(form);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  currentTask.push({
+    task: taskInput.value,
+    details: taskDetailInput.value,
+    imp: taskCheckbox.checked,
+  });
+  localStorage.setItem("currentTask", JSON.stringify(currentTask));
+  taskInput.value = ''
+  taskDetailInput.value = ''
+  taskCheckbox.checked = ''
+  renderTask()
+});
+
+

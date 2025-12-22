391870e048295a88c660563f74679405f8adbee0
WebAsif1 <mdasif606392@gmail.com>
2025-12-22 23:42:40 +0600

    day 71 props and components

---
+++ day 71 React JS F/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='h-30 w-30 bg-red-500'></div>
+    </>
+  )
+}
+
+export default App

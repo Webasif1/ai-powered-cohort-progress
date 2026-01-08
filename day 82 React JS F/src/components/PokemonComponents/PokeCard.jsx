b029e51a5c51e8cca6f0876d57f9f0404304f918
WebAsif1 <mdasif606392@gmail.com>
2026-01-08 23:55:26 +0600

    context

--- day 82 React JS F/src/components/PokemonComponents/PokeCard.jsx
+++ day 82 React JS F/src/components/PokemonComponents/PokeCard.jsx
@@ -5,12 +5,10 @@ const PokeCard = (props) => {
   const [image, setimage] = useState(props.image);
   const primaryType = props.type?.[0];
 
-
-
   return (
     <>
       <div key={props.name}>
-        <div className={`w-70 bg-zinc-700 rounded-2xl hover:-translate-y-3 transition-all duration-300 flex flex-col items-center py-5 shadow-white/20  hover:shadow-2xl border-2 border-${typeColors[primaryType] || "border-gray-300"} cursor-pointer`}>
+        <div className={`w-70 bg-zinc-700 rounded-2xl hover:-translate-y-3 transition-all duration-300 flex flex-col items-center py-5 shadow-white/20  hover:shadow-2xl border-2 ${typeColors[primaryType] || "border-gray-300"} cursor-pointer`}>
           <p className={`text-zinc-300 text-xl my-3 mb-4 py-1 px-3 bg-white/30 w-fit rounded-full`}>
             #{props.id}
           </p>
@@ -23,7 +21,7 @@ const PokeCard = (props) => {
             }}
             className={`rounded-full relative`}
           >
-            <div className={`bg-${typeColors[primaryType]} z-0 w-30 h-30 rounded-full blur-3xl top-0 left-0 absolute`}></div>
+            <div className={`${typeColors[primaryType]} z-0 w-30 h-30 rounded-full blur-3xl top-0 left-0 absolute`}></div>
             <img
               className="w-30 h-30 relative z-10 hover:scale-80"
               src={image || "/placeholder.png"}
@@ -55,7 +53,7 @@ const PokeCard = (props) => {
           <p className="text-md mt-5 flex items-center gap-2">
             Type:
             <span className="flex items-center gap-1">
-              {props.type.map((elem, idx) => <p key={idx} className="text-md font-semibold py-1 px-2 bg-white/20 rounded-full">{elem}</p>)}
+              {props.type.map((elem, idx) => <small key={idx} className="text-md font-semibold py-1 px-2 bg-white/20 rounded-full">{elem}</small>)}
             </span>
           </p>
         </div>

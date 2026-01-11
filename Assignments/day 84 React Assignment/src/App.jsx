f8fe1818d54bdc7dd78cc69ab18af04bb543caa3
WebAsif1 <mdasif606392@gmail.com>
2026-01-11 22:53:47 +0600

    daily chalange

--- Assignments/day 84 React Assignment/src/App.jsx
+++ Assignments/day 84 React Assignment/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <>
       <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/> }/>
+      </Routes>
     </>
   )
 }
--- Assignments/day 84 React Assignment/src/Components/NavBar/NavBar.jsx
+++ Assignments/day 84 React Assignment/src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='navBar container'>
-        <div className="navLink">
-          <Link>Service</Link>
-          <Link>Work</Link>
-          <Link>About</Link>
+      <div className="navLink">
+        <Link className='navItem'>Service</Link>
+        <Link className='navItem'>Work</Link>
+        <Link className='navItem'>About</Link>
+      </div>
+      <Link to='/' className="NavLogo">
+        <img src="./favicon-1.png" alt="navLogo" />
+        <h2 className='logoText'>Astratto</h2>
+      </Link>
+      <div className="navBtns">
+        <Link to='/'>Careers</Link>
+        <Link className='contactUs-btn' to="/">Contact Us <i class="ri-arrow-right-up-long-line"></i> </Link>
+      </div>
+
+      {/* Mobile Menu  */}
+      <div className='mobileMenu'>
+        <div onClick={() => {
+          setToggleMenu((prev) => !prev)
+        }} className='mobileNav'>
+          {toggleMenu ? <i class="ri-close-large-line"></i> :
+          <i class="ri-menu-3-fill"></i>
+          }
         </div>
-        <Link to='/' className="NavLogo">
-          <img src="./favicon-1.png" alt="navLogo" />
-          <h2 className='logoText'>Astratto</h2>
-        </Link>
-          <div className="navBtns">
-            <Link to='/'>Careers</Link>
-            <Link className='contactUs-btn' to="/">Contact Us <i class="ri-arrow-right-up-long-line"></i> </Link>
+        {toggleMenu && <div className="mobileNavList">
+          <div className="mobNavLink">
+            <Link className='navItem'>Service</Link>
+            <Link className='navItem'>Work</Link>
+            <Link className='navItem'>About</Link>
+            <Link className='navItem' to='/'>Careers</Link>
+            <Link className='navItem contactUs-btn' to="/">Contact Us <i class="ri-arrow-right-up-long-line"></i> </Link>
           </div>
+        </div>}
+      </div>
     </div>
   )
 }

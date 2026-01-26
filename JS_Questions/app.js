69766a3cce9cb8fe5b9819d9e7aa750976bcaf37
WebAsif1 <mdasif606392@gmail.com>
2026-01-26 12:58:56 +0600

    JS_Questions solving 20 question solved

---
+++ JS_Questions/app.js
@@ -0,0 +1,137 @@
+// 1.Create two variables: one storing your full name and another storing your favorite hobby. Print a sentence in the format:
+//  "My name is <your name> and <your hobby>".
+
+
+// const fullName = prompt("Write your name 😎");
+// const favoriteHobby = prompt("White your favorite hobby 👍");
+// const string = `My name is ${fullName} and ${favoriteHobby}`
+
+// console.log(string);
+
+
+// 2. Perform the arithmetic calculation `45 * 2 - 10` and print the final result.
+
+// const result = 45*2-10;
+// console.log(result);
+
+
+// 3. Write a program that retrieves and prints the current year using JavaScript’s `Date` object.
+
+// const date = new Date();
+
+// let year = date.getFullYear()
+// console.log(year);
+
+// 4. Store your first name and last name in two different variables, then print your full name in a single output.
+
+// const firstName = "Md Arafat Hossain";
+// const lastName = "Asif";
+// console.log(firstName + " " + lastName);
+
+// 5. Create a variable with an initial value. Print its value, update the value, and print the updated value again.
+
+// let a =10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// 6. Print a custom error message using `console.error()`.
+
+
+// console.error('Something went wrong')
+
+// 7. Store a number in a variable and print the square of that number.
+
+// let a = 10;
+// a = a ** 2
+// console.log(a);
+
+// 8. Create a boolean variable and print its value.
+
+// const bool = true;
+// console.log(bool);
+
+// 9. Store your age in a variable and print whether your age is greater than 18.
+
+// let age = 23;
+
+// if(age > 18){
+//   console.log(age);
+// }else{
+//   console.log("Your age is less then 18");
+// }
+
+
+// 10. Divide `100` by `0` and print the result. Observe what JavaScript returns.
+
+// let a = 100 / 0;
+
+// console.log(a);
+
+//** ## Variables and Data types
+
+// 11. Create a variable using `let` and print its value.
+
+// let a = 10;
+// console.log(a);
+
+// 12. Declare a constant named `PI` with the value `3.14` and print it.
+
+// const PI = 3.14
+// console.log(PI);
+
+// 13. Create a variable, print its value, update the value, and print the updated value again.
+
+// let a = 10;
+// console.log(a);
+// a = 11;
+// console.log(a);
+
+// 14. Print the output of `typeof null` and observe what type JavaScript reports.
+
+// console.log(typeof null);
+
+// 15. Create a variable containing a numeric value as a string (e.g., `"25"`) and print its type.
+
+// const num = "25"
+// console.log(typeof num);
+
+
+// 16. Create a boolean variable and print its type using `typeof`.
+
+// const bool = false;
+// console.log(typeof bool);
+
+// 17. Create three variables: a string, a number, and a boolean. Print all of them together in a formatted output.
+
+// const nameOfUser = "Asif"
+// const age = 22;
+// const isCorrect = true;
+
+// console.log(`My name is ${nameOfUser} & my age is ${age} this is ${isCorrect}`);
+
+// 18. Declare a variable without assigning a value and print its type using `typeof`.
+
+// let val ; //show undefined
+// const val; //not correct show error
+// console.log(typeof val);
+
+// 19. Create a variable with the value `undefined` and print its type.
+
+// const type = undefined;
+// console.log(typeof type);
+// const type = NaN;
+// console.log(typeof type);
+// const type = null;
+// console.log(typeof type);
+
+// 20. Create an array using `const`, print it, attempt to reassign the entire array (and handle the error), then modify the existing array by adding a new element and print the updated array.
+// const arr = [12,13,14,15]
+// console.log(arr);
+// try{
+//   arr = [11,13,14,15]
+// } catch(e){
+//   console.error("Error: Cannot reassign a const array");
+// }
+// arr.push(16)
+// console.log(arr);

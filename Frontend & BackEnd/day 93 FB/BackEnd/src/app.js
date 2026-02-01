469b5bf74770e07753ea0cb3a104afe7d8b6ed94
WebAsif1 <mdasif606392@gmail.com>
2026-02-01 20:37:51 +0600

    fix(app): correct syntax and improve note API functionality and API has been created ,backend api done

--- Frontend & BackEnd/day 93 FB/BackEnd/src/app.js
+++ Frontend & BackEnd/day 93 FB/BackEnd/src/app.js
@@ -1,50 +1,75 @@
 /*Require Express from express */
-const express = require("express")
+const express = require("express");
 /**Import noteModel from ./models/note.model */
-const noteMOdel = require("./models/note.model")
+const noteMOdel = require("./models/note.model");
+const noteModel = require("./models/note.model");
 /*Create app variable */
-const app = express()
-
+const app = express();
 
 /**Middlewares */
 /**Request element from body */
-app.use(express.json())
+app.use(express.json());
 
-/**Create post API */
-app.post("/api/notes", async (req,res)=>{
+/**Create post API  for create note*/
+app.post("/api/notes", async (req, res) => {
   /**Bring data from body */
-  const {title,description} = req.body
+  const { title, description } = req.body;
 
   /**Create noteModel */
   const note = await noteMOdel.create({
     title,
-    description
-  })
+    description,
+  });
 
   /**Response Status */
   res.status(201).json({
-    message:"Note has been created",
-    note
-  })
-})
+    message: "Note has been created",
+    note,
+  });
+});
 
-/**Create get API */
-app.get("/api/notes", async (req,res)=>{
+/**Create get API for get data*/
+app.get("/api/notes", async (req, res) => {
   /**Fetch data from noteModel server*/
   const note = await noteMOdel.find();
 
   /**Send response */
   res.status(200).json({
-    message:"Note fetched successfully",
-    note
-  })
+    message: "Note fetched successfully",
+    note,
+  });
+});
 
-})
+/**Create delete API  for delete note*/
+app.delete("/api/notes/:id", async (req, res) => {
+  /**Get id form params */
+  const id = req.params.id;
 
-/**Create delete API */
-app.delete("/api/notes/:id", (req,res)=>{
+  /**Delete note by id */
+  await noteModel.findByIdAndDelete(id);
 
-})
+  /**Send response */
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+/**Create patch API  for update note*/
+app.patch("/api/notes/:id", async (req, res) => {
+  /**Get id form params */
+  const id = req.params.id;
+
+  /**Description request from body */
+  const { description } = req.body;
+
+  /**Update note by id */
+  await noteModel.findByIdAndUpdate(id, { description });
+
+  /**Send response */
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
 
 /** Export app in server.js file to run server */
-module.exports = app
+module.exports = app;

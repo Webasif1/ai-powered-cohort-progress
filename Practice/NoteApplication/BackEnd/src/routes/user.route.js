1cf17f9c14259df54706a6f1bcc90abab6e1fc42
WebAsif1 <mdasif606392@gmail.com>
2026-02-08 00:19:47 +0600

    Login route

--- Practice/NoteApplication/BackEnd/src/routes/user.route.js
+++ Practice/NoteApplication/BackEnd/src/routes/user.route.js
@@ -1,38 +1,38 @@
 /**Require express */
 const express = require("express");
 /**Require jwt */
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 /**Require cookie */
-const cookie = require("cookie")
+const cookie = require("cookie");
 /**Require crypto to hash password */
-const crypto = require("crypto")
+const crypto = require("crypto");
 /**Import userModel */
 const userModel = require("../models/user.model");
 
 /**create authRouter */
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 /**Create User Register api */
-authRouter.post("/register", async(req , res)=>{
+authRouter.post("/register", async (req, res) => {
   //Request name, email and password from body
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
   //isUserAlreadyExist
-  const isUserAlreadyExist = await userModel.findOne({email});
-  if(isUserAlreadyExist){
+  const isUserAlreadyExist = await userModel.findOne({ email });
+  if (isUserAlreadyExist) {
     return res.status(400).json({
-      message:"With this email user already exist"
-    })
+      message: "With this email user already exist",
+    });
   }
 
   //Create hash password
-  const hashPassword = crypto.createHash("md5").update(password).digest("hex")
+  const hashPassword = crypto.createHash("md5").update(password).digest("hex");
 
   //Create user
   const user = await userModel.create({
     name,
     email,
-    password:hashPassword
+    password: hashPassword,
   });
 
   //Jwt_token
@@ -42,17 +42,56 @@ authRouter.post("/register", async(req , res)=>{
       email: user.email,
     },
     process.env.JWT_SECRET
-  )
+  );
 
   //Cookie set
   res.cookie("jwt_token", token);
 
   //send status
   res.status(201).json({
-    message:"User registered successfully",
-    user
-  })
-})
+    message: "User registered successfully",
+    user,
+  });
+});
+
+/**Login route */
+authRouter.post("/login", async (req, res) => {
+  //Request email and password from body
+  const { email, password } = req.body;
+
+  //Find user based on email
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found with this email",
+    });
+  }
+
+  //Password Check
+  const isPasswordMatched =
+    user.password === crypto.createHash("md5").update(password).digest("hex");
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  //jwt-token
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET
+  );
+
+  //cookie
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User logged in",
+    user,
+  });
+});
 
 /** module.exports authRouter */
-module.exports = authRouter
+module.exports = authRouter;

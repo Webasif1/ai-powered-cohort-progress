e2c94f9787b76efcc675939058db9d4f4446d3ef
WebAsif1 <mdasif606392@gmail.com>
2026-02-18 02:29:25 +0600

    authRoutes and controllers created

--- BackEnd/day 105 BackEnd B/src/app.js
+++ BackEnd/day 105 BackEnd B/src/app.js
@@ -1,9 +1,25 @@
 /**Require express  */
 const express = require('express');
+/**Require cookie */
+const cookieParser = require("cookie-parser")
+
+/**Import authRouter from ./routes/auth.routes */
+const authRouter = require("./routes/auth.routes")
 
 /**Create app variable  */
 const app = express()
 
+/**
+ * Middleware
+ */
+app.use(express.json())
+app.use(cookieParser())
+
+
+/**
+ * Create /api/auth for authRouter
+ */
+app.use("/api/auth", authRouter)
 /**
  * -Module.exports app to run server in server.js
  */
---
+++ BackEnd/day 105 BackEnd B/src/controllers/auth.controller.js
@@ -0,0 +1,163 @@
+/**
+ * -Import userModel
+ */
+const userModel = require("../models/user.model");
+/**
+ * Require crypto
+ */
+const crypto = require("crypto");
+/**
+ * Require jsonwebtoken
+ */
+const jwt = require("jsonwebtoken");
+
+/**
+ * registerController
+ */
+async function registerController (req, res) {
+  // Bring data from body
+  const { username, email, password, bio, profilePicture } = req.body;
+
+  /*** =====Bad Code===
+   //Check isUserExistingByEmail
+  const isUserExistingByEmail = await userModel.findOne({email})
+
+  //Condition for create user email
+  if(isUserExistingByEmail){
+    return res.status(409).json({
+      message: "User already exist with this email"
+    })
+  }
+
+  //Check isUserExistingByUsername
+  const isUserExistingByUsername = await userModel.findOne({username})
+
+  //Condition for create user username
+  if(isUserExistingByUsername){
+    return res.status(409).json({
+      message:"User already exist with this username"
+    })
+  }
+  */
+
+  //Check isUserAlreadyExist
+  const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  //Condition
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message:
+        isUserAlreadyExist.email === email
+          ? "User already exist with this email"
+          : "User already exist with this username",
+    });
+  }
+
+  //Hash password
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  //Create user
+  const user = await userModel.create({
+    username,
+    email,
+    bio,
+    profilePicture,
+    password: hash, //Save password as a hash
+  });
+
+  //sign jwt token
+  /**
+   *user data
+   *data should be unique
+   */
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  //Set cookie
+  res.cookie("token",token)
+
+  //send status
+  res.status(201).json({
+    message:"User Register Successfully",
+    user:{
+      username:user.username,
+      email:user.email,
+      bio:user.bio,
+      profilePicture:user.profilePicture
+    }
+  })
+}
+
+
+/**
+ * loginControllers
+ */
+async function loginController (req,res){
+  //Bring data from body
+  const {username ,email ,password}= req.body
+
+  //Find user
+  const user = await userModel.findOne({
+    $or:[
+      {username:username},
+      {email:email}
+    ]
+  })
+
+  //if user not found
+  if(!user){
+    return res.status(404).json({
+      message:"User not found"
+    })
+  }
+
+  //if user found check hash password
+  const hash = crypto.createHash("sha256").update(password).digest("hex")
+  // compare hash pass to body pass
+  const isPasswordValid = hash === user.password
+  // if password is not valid
+  if(!isPasswordValid){{
+    return res.status(401).json({
+      message:"Invalid password"
+    })
+  }}
+
+  // if every think match create token
+  const token = jwt.sign(
+    {id:user._id},
+    process.env.JWT_SECRET,
+    {expiresIn:"1d"}
+  )
+
+  // set token in cookie
+  res.cookie("token",token)
+
+
+  // send status
+  res.status(200).json({
+    message:"User logged in successfully",
+    user:{
+      username:user.username,
+      email:user.email,
+      bio:user.bio,
+      profilePicture:user.profilePicture
+    }
+  })
+}
+
+
+/**
+ * module.exports
+ */
+
+module.exports = {
+  registerController,
+  loginController
+}

98e7a0bb7040e9107ed2a31b32a68eb99f9f9737
WebAsif1 <mdasif606392@gmail.com>
2026-02-19 00:19:35 +0600

    postModel

--- BackEnd/day 105 BackEnd B/src/controllers/post.controller.js
+++ BackEnd/day 105 BackEnd B/src/controllers/post.controller.js
@@ -1,6 +1,10 @@
 /***Import post model */
 const postModel = require("../models/post.model")
 /**
+ * jwt
+ */
+const jwt = require("jsonwebtoken")
+/**
  * REquire imageKit to cloud store image-video
  */
 const ImageKit = require("@imagekit/nodejs")
@@ -20,12 +24,22 @@ const imageKit = new ImageKit({
 async function createPostController(req,res){
   console.log(req.body, req.file)
 
-  const file = await imageKit.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer),"file"),
-    fileName:"Test"
-  })
+  const token = req.cookies.token
 
-  res.send(file)
+  if(!token){
+    return res.status(401).json({
+      message:"Token is not provided, Unauthorized access"
+    })
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+  // const file = await imageKit.files.upload({
+  //   file: await toFile(Buffer.from(req.file.buffer), "file"),
+  //   fileName:"Test"
+  // })
+
+  // res.send(file)
 }
 
 

99d08a7f6f45d5e58c557ed07dc7d76625115793
WebAsif1 <mdasif606392@gmail.com>
2026-02-28 22:22:19 +0600

    connected to db

--- FrontendBackEnd/day 113 FB/Backend/src/app.js
+++ FrontendBackEnd/day 113 FB/Backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cookie = require("cookie-parser")
 
 const app = express()
 
 app.use(express.json())
+app.use(cookie())
 
 module.exports = app
---
+++ FrontendBackEnd/day 113 FB/Backend/src/config/database.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+async function connectToDb(){
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to db");
+  }
+  catch(err){
+    console.log("Error Connecting To DB", err);
+  }
+}
+
+module.exports = connectToDb

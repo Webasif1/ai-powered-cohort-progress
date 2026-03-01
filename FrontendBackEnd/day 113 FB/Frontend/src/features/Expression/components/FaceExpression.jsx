bfe9f5179b3e26553ceea9499f447b81939fd757
WebAsif1 <mdasif606392@gmail.com>
2026-03-01 13:55:19 +0600

    refactor code and follow folder structure

--- FrontendBackEnd/day 113 FB/Frontend/src/features/Expression/components/FaceExpression.jsx
+++ FrontendBackEnd/day 113 FB/Frontend/src/features/Expression/components/FaceExpression.jsx
@@ -1,70 +1,18 @@
 import { useEffect, useRef, useState } from "react";
-import {
-  FaceLandmarker,
-  FilesetResolver
-} from "@mediapipe/tasks-vision";
+import { detect, init } from "../utils/utils";
+
 export default function FaceExpression() {
   const videoRef = useRef(null);
   const landmarkerRef = useRef(null);
   const animationRef = useRef(null);
+  const streamRef = useRef(null)
+
   const [expression, setExpression] = useState("Detecting...");
 
+
   useEffect(() => {
-    let stream;
-    const init = async () => {
-      const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-      );
-      landmarkerRef.current = await FaceLandmarker.createFromOptions(
-        vision,
-        {
-          baseOptions: {
-            modelAssetPath:
-              "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
-          },
-          outputFaceBlendshapes: true,
-          runningMode: "VIDEO",
-          numFaces: 1,
-        }
-      );
-      stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      videoRef.current.srcObject = stream;
-      await videoRef.current.play();
-      detect();
-    };
-    const detect = () => {
-      if (!landmarkerRef.current || !videoRef.current) return;
-      const results = landmarkerRef.current.detectForVideo(
-        videoRef.current,
-        performance.now()
-      );
-      if (results.faceBlendshapes?.length > 0) {
-        const blendshapes = results.faceBlendshapes[0].categories;
-        const getScore = (name) =>
-          blendshapes.find((b) => b.categoryName === name)?.score || 0;
-        const smileLeft = getScore("mouthSmileLeft");
-        const smileRight = getScore("mouthSmileRight");
-        const jawOpen = getScore("jawOpen");
-        const browUp = getScore("browInnerUp");
-        const frownLeft = getScore("mouthFrownLeft");
-        const frownRight = getScore("mouthFrownRight");
-        let currentExpression = "Neutral";
-        if (smileLeft > 0.5 && smileRight > 0.5) {
-          currentExpression = "Happy 😄";
-        } else if (jawOpen > 0.3 && browUp > 0.2) {
-          currentExpression = "Surprised 😲";
-        } else if (frownLeft > 0.0002 && frownRight > 0.0002) {
-          currentExpression = "Sad 😢";
-        }
-        setExpression(currentExpression);
-      }
-      animationRef.current = requestAnimationFrame(detect);
-    };
-    init();
+    init({ landmarkerRef, videoRef, streamRef });
     return () => {
-      if (animationRef.current) {
-        cancelAnimationFrame(animationRef.current);
-      }
       if (landmarkerRef.current) {
         landmarkerRef.current.close();
       }
@@ -83,6 +31,7 @@ export default function FaceExpression() {
         playsInline
       />
       <h2>{expression}</h2>
+      <button onClick={() => { detect({ landmarkerRef, videoRef, setExpression }) }} style={{ border: "none", outline: "none", padding: "15px", borderRadius: "5px", cursor: "pointer", fontSize: "16px", }}>Detect Expression</button>
     </div>
   );
 }

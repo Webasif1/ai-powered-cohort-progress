19207f542f2eb759a49609e3d48d44b2a3b221cf
WebAsif1 <mdasif606392@gmail.com>
2026-03-12 20:11:48 +0600

    Send mail successfully using nodemailer

--- BackEnd/day 120 Backend B/src/app.js
+++ BackEnd/day 120 Backend B/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth.route";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
--- BackEnd/day 120 Backend B/src/controllers/auth.controller.js
+++ BackEnd/day 120 Backend B/src/controllers/auth.controller.js
@@ -1,21 +1,45 @@
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { sendEmail } from "../services/mail.service.js";
 
-export async function register(res, req) {
+export async function register(req, res) {
   const { username, email, password } = req.body;
 
   const isUserAlreadyExist = await userModel.findOne({
-    $or: {
-      username,
-      email,
-    },
+    $or: [{ username }, { email }],
   });
 
-  if(isUserAlreadyExist){
+  if (isUserAlreadyExist) {
     return res.status(409).json({
-      message:"With this email or username user already exist",
-      success:false,
-      err: "User already exist"
-    })
+      message: "With this email or username user already exist",
+      success: false,
+      err: "User already exist",
+    });
   }
+
+  const user = await userModel.create({ username, email, password });
+
+  await sendEmail({
+    to: email,
+    subject: "Welcome to perplexity",
+    text: `Welcome ${username}`,
+    html: `<h2>Welcome to Perplexity</h2>
+          <p>Hi ${username},</p>
+
+          <p>
+          Thanks for registering. Your account has been successfully created.
+          </p>
+
+          <hr/>
+          <p>Perplexity Team</p>`,
+  });
+
+  res.status(200).json({
+    message: "User has been created successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }

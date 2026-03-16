c9b3c0a4f12e9f49f1cd3fa7cbc5999db56b610e
WebAsif1 <mdasif606392@gmail.com>
2026-03-17 03:08:53 +0600

    socket.io implement

--- FrontendBackEnd/dayf 124 FB AI/Backend/server.js
+++ FrontendBackEnd/dayf 124 FB AI/Backend/server.js
@@ -1,15 +1,19 @@
-import "dotenv/config"
+import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const PORT = process.env.PORT || 8000;
+const httpServer = http.createServer(app);
 
+initSocket(httpServer);
 
 connectDB().catch((err) => {
   console.error("Mongodb connection failed:", err);
   process.exit(1);
 });
 
-app.listen(PORT, () => {
-  console.log("The server is running on port: "+ PORT);
+httpServer.listen(PORT, () => {
+  console.log("The server is running on port: " + PORT);
 });

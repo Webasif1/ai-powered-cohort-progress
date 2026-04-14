fe22b6d03a176cfe39c870ac9ff4345df0254a15
WebAsif1 <mdasif606392@gmail.com>
2026-04-15 00:59:19 +0600

    implement product creation with image upload functionality

--- FrontendBackEnd/day 142 snitch/Backend/src/controllers/product.controller.js
+++ FrontendBackEnd/day 142 snitch/Backend/src/controllers/product.controller.js
@@ -1,6 +1,33 @@
 import produntModel from "../models/product.models.js";
+import { uploadFile } from "../services/store.service.js";
 
 export async function createProductController(req, res) {
+  const { title, description, price } = req.body;
+  const seller = req.user;
+  const images = await Promise.all(
+    req.files.map(async (file) => {
+      return await uploadFile({
+        buffer: file.buffer,
+        fileName: file.originalname,
+      });
+    }),
+  );
   try {
-  } catch (error) {}
+    const product = await produntModel.create({
+      title,
+      description,
+      price: {
+        amount: price,
+        currency: "INR",
+      },
+      seller: seller._id,
+      images,
+    });
+    return res
+      .status(201)
+      .json({ message: "Product created successfully", product });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
--- FrontendBackEnd/day 142 snitch/Backend/src/routes/product.routes.js
+++ FrontendBackEnd/day 142 snitch/Backend/src/routes/product.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { authenticateSeller } from "../middleware/auth.middleware.js";
 import { createProductController } from "../controllers/product.controller.js";
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
 
 const router = Router();
 
-router.post("/", authenticateSeller, createProductController);
+router.post(
+  "/",
+  authenticateSeller,
+  upload.array("images", 5),
+  createProductController,
+);
 
 export default router;

b370fe22771986ed191103127965219d215b8bbf
WebAsif1 <mdasif606392@gmail.com>
2026-06-03 12:33:23 +0600

    Navbar in next js app

--- Next_js/next-project02/src/components/Navbar.js
+++ Next_js/next-project02/src/components/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = () => {
         <Link href="/contact">Contact</Link>
       </span>
 
+      <button className='bg-gray-800 text-white px-4 py-2 rounded-md cursor-pointer'>Contact Now</button>
     </div>
   )
 }

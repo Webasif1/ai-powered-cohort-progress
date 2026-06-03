e85b47df9540023b11fe2c71dad253b6c278cb4d
WebAsif1 <mdasif606392@gmail.com>
2026-06-03 12:53:18 +0600

    set up basic e-com next server and call auth api

---
+++ Next_js/next-project-e-com/src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Create Zewar E-Commerce App",
+  description: "It is a simple e-commerce app built with Next.js and Tailwind CSS. It is a simple e-commerce app built with Next.js and Tailwind CSS.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}

72d7635c42e25e9118a8d51ea9b0267d3e8de1bc
WebAsif1 <mdasif606392@gmail.com>
2026-06-16 00:51:26 +0600

    Register route complete

--- TypeScript/resumebuilder/src/app/api/auth/register/routes.ts
+++ TypeScript/resumebuilder/src/app/api/auth/register/routes.ts
@@ -5,54 +5,81 @@ import { ApiResponse } from "@/types/api.types";
 import { RegisterBody } from "@/types/user.types";
 import { NextRequest, NextResponse } from "next/server";
 
-async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     await connectDB();
     const body: RegisterBody = await req.json();
 
-    const {name, email, mobile,password} = body;
+    const { name, email, mobile, password } = body;
 
-    if(!name || !email || !password){
-      return NextResponse.json<ApiResponse>({
-        success:false,
-        message:"All fields are required"
-      },{
-        status:400
-      })
+    if (!name || !email || !password) {
+      return NextResponse.json<ApiResponse>(
+        {
+          success: false,
+          message: "All fields are required",
+        },
+        {
+          status: 400,
+        },
+      );
     }
 
-    const isExist = await userModel.findOne({email});
+    const isExist = await userModel.findOne({ email });
 
-    if(isExist){
-      return NextResponse.json<ApiResponse>({
-        success:false,
-        message: "This user Already exist"
-      },{
-        status:409
-      })
+    if (isExist) {
+      return NextResponse.json<ApiResponse>(
+        {
+          success: false,
+          message: "This user Already exist",
+        },
+        {
+          status: 409,
+        },
+      );
     }
 
     const newUser = await userModel.create({
-      name,email,mobile,password
-    })
+      name,
+      email,
+      mobile,
+      password,
+    });
 
-    const token = generateToken({userID: newUser._id})
+    const token = generateToken({ userID: newUser._id });
 
-    const response =NextResponse.json<ApiResponse>({
-      success:true,
-      message:"User register successfully",
-      data:{
-        user:{
-          _id: newUser._id,
-          name:newUser.name,
-          email:newUser.email
-        }
-      }
-    },{
-      status:201
-    })
+    const response = NextResponse.json<ApiResponse>(
+      {
+        success: true,
+        message: "User register successfully",
+        data: {
+          user: {
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+          },
+        },
+      },
+      {
+        status: 201,
+      },
+    );
 
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 1000,
+    });
   } catch (error) {
     console.log("error in register api", error);
+    return NextResponse.json<ApiResponse>(
+      {
+        success: false,
+        message: "Something went wrong",
+        error: {
+          error,
+        },
+      },
+      { status: 500 },
+    );
   }
 }

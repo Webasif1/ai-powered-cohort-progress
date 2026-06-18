9b12cfd7d703adcd2589eaea3d5320de8ad8af77
WebAsif1 <mdasif606392@gmail.com>
2026-06-18 23:18:00 +0600

    all resume types interface done

--- TypeScript/resumebuilder/src/types/resume.types.ts
+++ TypeScript/resumebuilder/src/types/resume.types.ts
@@ -1,9 +1,45 @@
-export interface IPersonalInfo{
-  fullName:string;
-  email:string;
-  profile:string;
-  location:string;
-  github:string;
-  linkedin:string;
-  portfolio:string;
+import { Types } from "mongoose";
+
+export interface IPersonalInfo {
+  fullName: string;
+  email: string;
+  profile: string;
+  location: string;
+  github: string;
+  linkedin: string;
+  portfolio: string;
+}
+
+export interface IWorkExperience {
+  company: string;
+  position: string;
+  statDate: string;
+  description: string;
+}
+
+export interface IProjects {
+  title: string;
+  description: string;
+  githubUrl: string;
+  linkedinUrl: string;
+  techStack: string[];
+}
+
+export interface IEducation {
+  institute: string;
+  degree: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface IResume {
+  _id?: string;
+  user_id: Types.ObjectId;
+  title: string;
+  summery: string;
+  personalInfo: IPersonalInfo;
+  workExperience?: IWorkExperience[];
+  projects: IProjects[];
+  education: IEducation[];
+  certifications?: string[];
 }

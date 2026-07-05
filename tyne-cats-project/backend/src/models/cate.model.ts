482e4ff7d986ba3188093d06dcd5c6aea3ccf4fd
WebAsif1 <mdasif606392@gmail.com>
2026-07-05 19:54:31 +0600

    Define cat model with schema and type definitions in cate.model.ts

---
+++ tyne-cats-project/backend/src/models/cate.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import type { ICat } from "../types/cates.types.ts";
+
+const catSchema = new mongoose.Schema<ICat>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    breed: {
+      type: String,
+      required: [true, "Breed is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    lifeSpan: {
+      type: Number,
+      required: [true, "LifeSpan is required"],
+    },
+    energyLevel: {
+      type: String,
+      required: [true, "Energy Level is required"],
+    },
+    kidsFriendly: {
+      type: Boolean,
+      default: true,
+    },
+    apartmentFriendly: {
+      type: Boolean,
+      default: true,
+    },
+    image: String,
+    color: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const CatModel = mongoose.model("Cat", catSchema);
+
+export default CatModel

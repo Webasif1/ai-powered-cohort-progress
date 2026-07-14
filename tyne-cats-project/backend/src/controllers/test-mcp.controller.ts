012691d42f666bd8ed94ed4487b75fe8664e087a
WebAsif1 <mdasif606392@gmail.com>
2026-07-14 22:08:00 +0600

    refactor test MCP controller for improved code readability and formatting

--- tyne-cats-project/backend/src/controllers/test-mcp.controller.ts
+++ tyne-cats-project/backend/src/controllers/test-mcp.controller.ts
@@ -1,21 +1,23 @@
 import type { Request, Response } from "express";
-import { getMcpClient } from "../services/mcp.srvice.ts"
+import { getMcpClient } from "../services/mcp.srvice.ts";
 
-export const testMcpTestController = async(req:Request, res:Response) =>{
+export const testMcpTestController = async (req: Request, res: Response) => {
   const client = await getMcpClient();
 
   const tools = await client.listTools();
 
   const result = await client.callTool({
-    name:"recommend_cats",
-    arguments:{
+    name: "recommend_cats",
+    arguments: {
       kidsFriendly: true,
-      apartmentFriendly:false
-    }
-  })
+      apartmentFriendly: false,
+    },
+  });
+
+  let catsData = result.content[0].text;
 
   return res.json({
-    success:true,
-    result
-  })
-}
+    success: true,
+    data: catsData,
+  });
+};
